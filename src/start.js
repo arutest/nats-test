e56c2836453f040685b2e388b1e98e9768fbe543
Andres Rojas <arutest@hotmail.com>
2020-07-20 10:44:23 -0400

    Elimina comentarios

--- src/start.js
+++ src/start.js
@@ -2,22 +2,14 @@
 
 const stan = require('./util/nats') // ARU
 const gt = require('./util/getTimes')  // ARU
+
 const nameQueue = 'orionX'
 const objMsg = { id:'uiyrtuertiue', name:'datos', valor:75856.78 }
-// const mymsg = '(ARU)' + JSON.stringify(objMsg) + '//' + gt.getDates()+ '//' + gt.getTimes()
 const mymsg = `(ARU)  ${JSON.stringify(objMsg)}//${gt.getDates()}//${gt.getTimes()}`
 
 stan.on('connect',() => {
   console.log('Conectado a Nats')
-//   for(var i=1;i<=5;i++){ 
-//     stan.publish(nameQueue, mymsg + '//' + i, (err,guid) => {
-//       if (err) {
-//         console.log('Falló la publicacion')
-//       } else {
-//         console.log('Publicado exitosamente en ', guid, 'cola : ', nameQueue)
-//       }
-//   })
-// } // for
+
 // Subscriber can specify how many existing messages to get.
 const opts = stan.subscriptionOptions().setStartWithLastReceived()
 const subscription = stan.subscribe(nameQueue, opts)
@@ -25,18 +17,6 @@ const subscription = stan.subscribe(nameQueue, opts)
 subscription.on('message', (msg) => {
   console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData() + ' desde cola ' + nameQueue)
 })
-
-// mymsg2 = `(ARU) OTRO MENSAJE//${gt.getDates()}//${gt.getTimes()}`
-// setTimeout(() =>{ 
-//   stan.publish(nameQueue, mymsg2 + '//***fuera****',  (err,guid) => {
-//   if (err) {
-//     console.log('Falló la publicacion *****')
-//   } else {
-//     console.log('Publicado exitosamente en ***** ', guid, 'msg =', mymsg , ' ***** cola : ', nameQueue)
-//   }
-// })
-// }
-// ,5*1000)
 
 // After 3 second, unsubscribe, when that is done, close the connection
 setTimeout(() => {
@@ -47,11 +27,6 @@ setTimeout(() => {
   })
 }, 45 * 1000)
 })
-
-// setInterval(() => { 
-//   console.log('Aru ---> ...', inter) 
- 
-// },3*1000)
 
 process.on('SIGINT', () => stan.close())
 process.on('SIGTERM', () => stan.close())
